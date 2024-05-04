5f7c6da79ef3b2308cec519b6dc44f57a8e35f12
Abhijat05 <abhijatsinha2004@gmail.com>
2024-05-04 16:14:02 +0530

    added mouse cursor effect

--- script.js
+++ script.js
@@ -11,3 +11,50 @@ gsap.from("li", {
     delay: 0.5,
     stagger: 0.2
 })
+
+var body = document.querySelector("body")
+var cursor = document.querySelector(".cursor")
+
+body.addEventListener("mousemove", function (dets) {
+    gsap.to(cursor, {
+        x: dets.x - 7,
+        y: dets.y - 2,
+        duration: 1,
+        ease: "power3.out"
+    })
+})
+
+var logo = document.querySelector(".logo")
+
+logo.addEventListener("mouseenter", function () {
+    gsap.to(cursor, {
+        scale: 4,
+        backgroundColor: "#ec4899"
+
+    })
+})
+
+logo.addEventListener("mouseleave", function () {
+    gsap.to(cursor, {
+        scale: 1,
+        backgroundColor: "white"
+    })
+})
+
+var navItems = document.querySelectorAll(".navbar ul li");
+
+navItems.forEach(function (navItem) {
+    navItem.addEventListener("mouseenter", function () {
+        gsap.to(cursor, {
+            scale: 3,
+            backgroundColor: "#16f2b3"
+        })
+    })
+
+    navItem.addEventListener("mouseleave", function () {
+        gsap.to(cursor, {
+            scale: 1,
+            backgroundColor: "white"
+        })
+    })
+})
