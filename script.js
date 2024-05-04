2652b310c28ed72d642394b6e7241485c273e1be
Abhijat05 <abhijatsinha2004@gmail.com>
2024-05-04 14:04:14 +0530

    added gsap animation

---
+++ script.js
@@ -0,0 +1,13 @@
+gsap.from(".logo", {
+    y: -30,
+    opacity: 0,
+    duration: 1,
+    delay: 0.5,
+})
+gsap.from("li", {
+    y: -30,
+    opacity: 0,
+    duration: 1,
+    delay: 0.5,
+    stagger: 0.2
+})
