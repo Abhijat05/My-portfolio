259f468718fc49e9af123ed9417289e976edc723
Abhijat05 <abhijatsinha2004@gmail.com>
2024-10-13 20:01:33 +0530

    Remove counter.js and javascript.svg

--- main.js
+++ main.js
@@ -1,24 +1,121 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// Animations with GSAP
+gsap.from(".logo", {
+  y: -30,
+  opacity: 0,
+  duration: 1,
+  delay: 0.5,
+});
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+gsap.from("li", {
+  y: -30,
+  opacity: 0,
+  duration: 1,
+  delay: 0.5,
+  stagger: 0.2,
+});
 
-setupCounter(document.querySelector('#counter'))
+gsap.from(".main .left", {
+  x: -30,
+  opacity: 0,
+  duration: 1,
+  delay: 1.5,
+});
+gsap.from(".main .main-box", {
+  x: 40,
+  opacity: 0,
+  duration: 1,
+  delay: 1.5,
+});
+
+gsap.from(".left-1 > div", {
+  y: -30,
+  opacity: 0,
+  duration: 1,
+  delay: 2,
+  stagger: 0.2,
+});
+
+gsap.from("button", {
+  y: -30,
+  opacity: 0,
+  duration: 1,
+  delay: 2.5,
+  stagger: 0.2,
+});
+
+// Custom cursor movement
+const body = document.querySelector("body");
+const cursor = document.querySelector(".cursor");
+
+body.addEventListener("mousemove", (event) => {
+  gsap.to(cursor, {
+      x: event.clientX - cursor.offsetWidth / 2,
+      y: event.clientY - cursor.offsetHeight / 2,
+      duration: 1,
+      ease: "power3.out",
+  });
+});
+
+// Custom cursor effects on logo hover
+const logo = document.querySelector(".logo");
+
+logo.addEventListener("mouseenter", () => {
+  gsap.to(cursor, {
+      scale: 4,
+      backgroundColor: "#ec4899",
+      duration: 0.3,
+      ease: "power3.out",
+  });
+});
+
+logo.addEventListener("mouseleave", () => {
+  gsap.to(cursor, {
+      scale: 1,
+      backgroundColor: "white",
+      duration: 0.3,
+      ease: "power3.out",
+  });
+});
+
+// Custom cursor effects on navbar items hover
+const navItems = document.querySelectorAll(".navbar ul li");
+
+navItems.forEach((navItem) => {
+  navItem.addEventListener("mouseenter", () => {
+      gsap.to(cursor, {
+          scale: 3,
+          backgroundColor: "#16f2b3",
+          duration: 0.3,
+          ease: "power3.out",
+      });
+  });
+
+  navItem.addEventListener("mouseleave", () => {
+      gsap.to(cursor, {
+          scale: 1,
+          backgroundColor: "white",
+          duration: 0.3,
+          ease: "power3.out",
+      });
+  });
+});
+
+// Custom cursor effects on .left-1 section hover
+const left1 = document.querySelector(".left-1");
+
+left1.addEventListener("mouseenter", () => {
+  gsap.to(cursor, {
+      opacity: 0,
+      duration: 0.3,
+      ease: "power3.out",
+  });
+});
+
+left1.addEventListener("mouseleave", () => {
+  gsap.to(cursor, {
+      opacity: 1,
+      duration: 0.3,
+      ease: "power3.out",
+  });
+});
+
