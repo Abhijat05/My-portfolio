1b99074ee43e2ed7b98bfdcbf5f06b505c41415b
Abhijat05 <abhijatsinha2004@gmail.com>
2025-03-13 23:30:41 +0530

    feat: Enhance index.html and CSS for improved theming and SEO optimization

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,36 @@ export default {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'typewriter': 'typewriter 2s steps(11) forwards',
+        'caret': 'typewriter 2s steps(11) forwards, blink 1s steps(11) infinite 2s',
+      },
+      keyframes: {
+        typewriter: {
+          to: {
+            left: '100%',
+          },
+        },
+        blink: {
+          '0%': {
+            opacity: '0',
+          },
+          '0.1%': {
+            opacity: '1',
+          },
+          '50%': {
+            opacity: '1',
+          },
+          '50.1%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '0',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
